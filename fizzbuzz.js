fa054782e4d044c5b09b639a0f00a0e985e6f3c2
Eva Maina <evajohnson714@gmail.com>
2020-06-10 13:53:52 +0300

    integrates eslint

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,30 +1,31 @@
+/* eslint-disable no-plusplus */
 /**
  * FizzBuzz Generator
  * @param {number} n - The number of times to 'print' Fizz, Buzz or FizzBuzz
  * @return {Array.<string>} Fizz, Buzz or FizzBuzz.
  */
 const fizzBuzz = function (n) {
-    let result = [];
+  const result = [];
 
-    for (let i = 1; i <= n; i++) {
-        let str = '';
+  for (let i = 1; i <= n; i++) {
+    let str = '';
 
-        if (i % 3 === 0) {
-            str += 'Fizz';
-        }
-
-        if (i % 5 === 0) {
-            str += 'Buzz';
-        }
-
-        if (!str) {
-            str += i;
-        }
-
-        result.push(str);
+    if (i % 3 === 0) {
+      str += 'Fizz';
     }
 
-    return result;
+    if (i % 5 === 0) {
+      str += 'Buzz';
+    }
+
+    if (!str) {
+      str += i;
+    }
+
+    result.push(str);
+  }
+
+  return result;
 };
 
 // console.log(fizzBuzz(50));
--- test.js
+++ test.js
@@ -1,29 +1,30 @@
+/* eslint-disable no-undef */
 const assert = require('assert');
 const fizzBuzz = require('./fizzbuzz');
 
-describe('FizzBuzz', function() {
+describe('FizzBuzz', () => {
   // describe('#indexOf()', function() {
-    const num = 100;
-    const result = fizzBuzz(num); // fizzbuzz returns a list
-    
-    it('should return a list of the same length as input', function() {
-      assert.equal(result.length, num);
-    });
+  const num = 100;
+  const result = fizzBuzz(num); // fizzbuzz returns a list
 
-    it('should return Fizz for numbers only divisible by 3', function() {
-      assert.equal(result[2], "Fizz");
-    });
+  it('should return a list of the same length as input', () => {
+    assert.equal(result.length, num);
+  });
 
-    it('should return Buzz for numbers only divisible by 5', function() {
-      assert.equal(result[4], "Buzz");
-    });
+  it('should return Fizz for numbers only divisible by 3', () => {
+    assert.equal(result[2], 'Fizz');
+  });
 
-    it('should return FizzBuzz for numbers divisible by 5 and 3', function() {
-      assert.equal(result[14], "FizzBuzz");
-    });
+  it('should return Buzz for numbers only divisible by 5', () => {
+    assert.equal(result[4], 'Buzz');
+  });
 
-    it('should return the number if not divisible by both 3 and 5', function() {
-      assert.equal(result[15], "16");
-    });
+  it('should return FizzBuzz for numbers divisible by 5 and 3', () => {
+    assert.equal(result[14], 'FizzBuzz');
+  });
+
+  it('should return the number if not divisible by both 3 and 5', () => {
+    assert.equal(result[15], '16');
+  });
   // });
 });
